71bba0669a1152498fe7116c65a41815b92c81c5
indushre2024 <indushreew01@gmail.com>
2025-09-19 10:03:53 -0400

    Todo items are draggable and rearranging the tasks is now possible

--- 02-todo-app/script.js
+++ 02-todo-app/script.js
@@ -3,7 +3,6 @@ const form = document.querySelector('.new-todo');
 const todo_input = document.getElementById('todo');
 const todo_list = document.querySelector('.todos');
 
-
 display_todos();
 function saveTodos(){
     localStorage.setItem('todos', JSON.stringify(todos));
@@ -11,6 +10,7 @@ function saveTodos(){
 
 function create_todo_item(todo, todo_id){
     const li_item = document.createElement('li');
+    li_item.draggable=true;
     li_item.innerHTML = `<input type="checkbox" id=${todo_id} name=${todo_id} ${todo.complete==true?'checked':''}>
                 <label for=${todo_id} class = "custom-checkbox">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="transparent" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>
@@ -54,5 +54,46 @@ form.addEventListener('submit', function(e){
     todo_input.value = '';
 });
 
+let todoItems = [...document.querySelectorAll(".todos li")];
+let oInd;
+let endInd;
 
+todoItems.forEach(card=>{
+    card.addEventListener("dragstart",e=>{
+        oInd = todoItems.indexOf(card);
+        card.classList.add("dragging");
+    });
+    card.addEventListener("dragend",e=>{
+        card.classList.remove("dragging");
+        todoItems = [...document.querySelectorAll(".todos li")];
+        endInd = todoItems.indexOf(card);
+        const content = todos.splice(oInd,1)[0];
+        todos.splice(endInd,0,content);
+        saveTodos();
+    });
+});
+
+todo_list.addEventListener("dragover",e=>{
+    e.preventDefault();
+    const targetTodo = getBelowTodo(e.clientY).element || undefined;
+    const drag = document.querySelector(".dragging");
+    if(targetTodo){
+        todoItems = [...document.querySelectorAll(".todos li")];
+        let ind = todoItems.indexOf(targetTodo);
+        todo_list.insertBefore(drag,targetTodo);
+    }else todo_list.append(drag);
+})
+
+function getBelowTodo(y){
+    const todoCards = [...document.querySelectorAll(".todos li:not(.dragging)")];
+    const target = todoCards.reduce((result, todo)=>{
+        const box = todo.getBoundingClientRect();
+        let offset = y - box.y - box.height/2;
+        if(offset<0 && offset>result.offset){
+            result = {offset:offset,element:todo};
+        }
+        return result;
+    },{offset:Number.NEGATIVE_INFINITY})
+    return target;
+}
     
