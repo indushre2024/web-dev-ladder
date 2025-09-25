cda5f00cecec961de96f0b022f19e27e502c81fe
indushre2024 <indushreew01@gmail.com>
2025-09-24 20:17:38 -0400

    Complete working code for Tic-Tac-Toe

--- 02-tictactoe-game/app.js
+++ 02-tictactoe-game/app.js
@@ -2,6 +2,24 @@ let isGameOver = false;
 let boxes = document.querySelectorAll(".box");
 const players = [...document.querySelectorAll(".player")];
 let currentPlayer;
+const startPage = document.querySelector(".start-page");
+const start = startPage.querySelector("button");
+
+start.addEventListener("click",e=>{
+    startPage.classList.add("hide");
+});
+resetGame();
+
+function resetGame(){
+    isGameOver = false;
+    boxes.forEach((box)=>{
+        box.textContent = "";
+        box.classList.remove("box-full");
+        box.classList.remove("box-highlight");
+    })
+    currentPlayer = players[Math.floor(Math.random()*2)];
+    currentPlayer.classList.add("player-highlight");
+}
 
 function swapPlayer(){
     currentPlayer.classList.remove("player-highlight");
@@ -9,16 +27,51 @@ function swapPlayer(){
     currentPlayer.classList.add("player-highlight");
 }
 
+function highlightBoxes(boxes){
+    boxes.forEach(box=>{
+        box.classList.add("box-highlight");
+    })
+}
+
 function checkGameOver(content){
-    if((boxes[0].textContent == content && boxes[1].textContent == content && boxes[2].textContent == content) ||
-        (boxes[3].textContent == content && boxes[4].textContent == content && boxes[5].textContent == content) ||
-        (boxes[6].textContent == content && boxes[7].textContent == content && boxes[8].textContent == content) ||
-       (boxes[0].textContent == content && boxes[3].textContent == content && boxes[6].textContent == content) ||
-        (boxes[1].textContent == content && boxes[4].textContent == content && boxes[7].textContent == content) ||
-        (boxes[2].textContent == content && boxes[5].textContent == content && boxes[8].textContent == content) ||
-        (boxes[0].textContent == content && boxes[4].textContent == content && boxes[8].textContent == content) ||
-        (boxes[2].textContent == content && boxes[4].textContent == content && boxes[6].textContent == content)
-    ) isGameOver = true;
+    const emptyBoxes = document.querySelectorAll(".box:not(.box-full)");
+    if(emptyBoxes.length==0){
+        isGameOver = true;
+        alert("Match is a draw, restarting the game");
+        resetGame();
+    }
+    if(boxes[0].textContent == content && boxes[1].textContent == content && boxes[2].textContent == content){
+        isGameOver = true;
+        highlightBoxes([boxes[0],boxes[1],boxes[2]]);
+    }
+    else if(boxes[3].textContent == content && boxes[4].textContent == content && boxes[5].textContent == content){
+        isGameOver = true;
+        highlightBoxes([boxes[3],boxes[4],boxes[5]]);
+    }
+    else if(boxes[6].textContent == content && boxes[7].textContent == content && boxes[8].textContent == content){
+        isGameOver = true;
+        highlightBoxes([boxes[6],boxes[7],boxes[8]]);
+    }
+    else if(boxes[0].textContent == content && boxes[3].textContent == content && boxes[6].textContent == content){
+        isGameOver = true;
+        highlightBoxes([boxes[0],boxes[3],boxes[6]]);
+    }
+    else if(boxes[1].textContent == content && boxes[4].textContent == content && boxes[7].textContent == content){
+        isGameOver = true;
+        highlightBoxes([boxes[1],boxes[4],boxes[7]]);
+    }
+    else if(boxes[2].textContent == content && boxes[5].textContent == content && boxes[8].textContent == content){
+        isGameOver = true;
+        highlightBoxes([boxes[2],boxes[5],boxes[8]]);
+    }
+    else if(boxes[0].textContent == content && boxes[4].textContent == content && boxes[8].textContent == content){
+        isGameOver = true;
+        highlightBoxes([boxes[0],boxes[4],boxes[8]]);
+    }
+    else if(boxes[2].textContent == content && boxes[4].textContent == content && boxes[6].textContent == content){
+        isGameOver = true;
+        highlightBoxes([boxes[2],boxes[4],boxes[6]]);
+    }
 }
 
 boxes.forEach(box=>{
@@ -28,20 +81,18 @@ boxes.forEach(box=>{
             box.classList.add("box-full");
             setTimeout(()=>{    
                 checkGameOver(currentPlayer.textContent);
-                if(isGameOver) alert(`Game Overrr!!! ${currentPlayer.textContent} Won`);
+                if(isGameOver) {
+                    setTimeout(()=>{
+                        resetGame();
+                        start.textContent = "Restart";
+                        startPage.querySelector("h1").textContent = "Lets Play Again!";
+                        startPage.classList.remove("hide");
+                    },1000);
+                };
                 swapPlayer();
             }, 100);  
         }
     })
 })
 
-function resetGame(){
-    isGameOver = false;
-    boxes.forEach((box)=>{
-        box.textContent = "";
-    })
-    currentPlayer = players[Math.floor(Math.random()*2)];
-    currentPlayer.classList.add("player-highlight");
-}
 
-resetGame();
